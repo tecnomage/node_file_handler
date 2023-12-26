72ec7c73f11106b799771d1af72b042fdd3d09cf
tecnomage <tecnocratay@gmail.com>
2023-12-26 17:16:18 -0300

    Refactor file upload and compression logic

--- controllers/db/index.js
+++ controllers/db/index.js
@@ -5,6 +5,7 @@ const path = require("path");
 const fs = require("fs");
 const zlib = require("zlib");
 
+//TODO1 retirar o uso do multer 
 const upload = multer({ dest: "uploads/" });
 
 const fileEndPoints = (app) => {
@@ -22,77 +23,31 @@ const fileEndPoints = (app) => {
 
   app.post("/sendfiles", upload.single("file"), (req, res) => {
     const file = req.file;
-    console.log("file", file);
-    
-const inputFilePath = file.path ; // Replace with actual file path
-const outputFilePath = 'compressed_file.txt.gz'; // Adjust as needed
 
-fs.createReadStream(inputFilePath)
-  .pipe(zlib.createGzip())
-  .pipe(fs.createWriteStream(outputFilePath))
-  .on('finish', () => {
-    console.log('File compressed successfully!');
-  })
-  .on('error', (err) => {
-    console.error('Compression error:', err);
-  });
+    // Create a gzip transform stream
+    const gzip = zlib.createGzip();
+    const source = fs.createReadStream(file.path);
+    const destination = fs.createWriteStream(`${file.path}.gz`);
 
-    
-    //const filePath = path.join(__dirname, "..", "uploads", file.filename);
+    // Pipe the file through the gzip stream
+    source.pipe(gzip).pipe(destination);
 
-    //console.log("file", filePath);
+    destination.on('finish', function() {
+        fs.unlink(file.path, (err) => {
+            if (err) {
+                console.error('Error deleting original file:', err);
+            }
+        });
 
-    // //Check if the file exists
-    // if (fs.existsSync(filePath)) {
-    //   // Compress the file
-    //   const compressedFilePath = filePath + ".gz";
-    //   const gzip = zlib.createGzip();
-    //   const input = fs.createReadStream(filePath);
-    //   const output = fs.createWriteStream(compressedFilePath);
-    //   input.pipe(gzip).pipe(output);
-
-    //   db.run(
-    //     "INSERT INTO files (file_path) VALUES (?)",
-    //     [compressedFilePath],
-    //     (err) => {
-    //       if (err) {
-    //         console.log("Error inserting file path", err);
-    //         res.status(500).json({ error: "Failed to insert file path" });
-    //       } else {
-    //         console.log("File path inserted");
-    //         res.json("File path inserted");
-    //       }
-    //     }
-    //   );
-    // } else {
-     
-    //   fs.readFile(inputImagePath, (err, buffer) => {
-    //     if (err) {
-    //       console.error(err);
-    //     } else {
-    //       sharp(buffer)
-    //         .jpeg({ quality: 50 }) // Adjust compression quality as needed
-    //         .toBuffer()
-    //         .then((compressedBuffer) => {
-    //           // Write the compressed Buffer to a file
-    //           fs.writeFile(outputImagePath, compressedBuffer, (err) => {
-    //             if (err) {
-    //               console.error(err);
-    //             } else {
-    //               console.log('Image compressed and written to file successfully!');
-    //             }
-    //           });
-    //         })
-    //         .catch((err) => {
-    //           console.error('Compression error:', err);
-    //         });
-    //     }
-    //   });
-    
-    
-    //}
-
-    res.json({ file: file });
+        // Save file information in the database
+        const compressedFilePath = `${file.path}.gz`;
+        db.run('INSERT INTO files(file_path) VALUES(?)', [compressedFilePath], function(err) {
+            if (err) {
+                return console.log(err.message);
+            }
+            res.json({ id: this.lastID, file_path: compressedFilePath });
+        });
+    });
   });
 };
 
